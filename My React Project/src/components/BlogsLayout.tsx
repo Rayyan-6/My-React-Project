5bbe9470686e0b56851c6ae415b6bcaf0216d7d6
Muhammad Rayyan <muhd.rayyan.mail@gmail.com>
2025-12-24 18:27:07 +0500

    tailwind initialised

--- My React Project/src/components/BlogsLayout.tsx
+++ My React Project/src/components/BlogsLayout.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 
+
 function BlogsLayout(){
     return(
         <>
         <h2>Latest Blogs</h2>
-        <button>React</button>
+        <button className="text-3xl">React</button>
         <button>Node</button>
         <Outlet />
         </>
