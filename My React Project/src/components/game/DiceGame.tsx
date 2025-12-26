93ea71e6c8030b6d74a518d9fe0211a5c672d341
Muhammad Rayyan <muhd.rayyan.mail@gmail.com>
2025-12-26 19:27:26 +0500

    dice clicks working

--- My React Project/src/components/game/DiceGame.tsx
+++ My React Project/src/components/game/DiceGame.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import Die from "./Die"
+import {nanoid} from 'nanoid'
+
 
 function DiceGame(){
     const [dice, setDice] = useState(generateAllDice())
@@ -9,26 +11,52 @@ function DiceGame(){
         console.log("Generate All Dice ran")
         return new Array(10)
         .fill(0)
-        .map(() =>{
-           
-            const val=Math.ceil(Math.random()*6)
-            return <Die value={val} isHeld={false}/>
-        } )
+        .map(() =>({
+            value: Math.ceil(Math.random()*6),
+            isHeld: false,
+            id: nanoid()
+        } ))
     }
+
+    function rollDice(){
+        return(generateAllDice())
+    }
+
+    function hold(id: string) {
+  setDice(oldDice =>
+    oldDice.map(die =>
+      die.id === id
+        ? { ...die, isHeld: !die.isHeld }
+        : die
+    )
+  )
+}
+
+
+    const diceElements = dice.map(dieObj=>{
+      return  <Die 
+
+        key={dieObj.id}
+        value={dieObj.value}
+        isHeld={dieObj.isHeld}
+        hold={() => hold(dieObj.id)}
+        
+        />
+    })
 
     return(
         <div className="dice-parent">
-        <div className="dice-row">
-          {dice}
-           
-        </div>
+            <div className="dice-row">
+            {diceElements}
+            
+            </div>
 
-        <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition" 
-        onClick={()=>{
-            setDice(generateAllDice())
-        }}
-        >
-            Roll</button>
+            <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition" 
+            onClick={()=>{
+                setDice(rollDice())
+            }}
+            >
+                Roll</button>
         </div>
     )
 }
--- My React Project/src/components/game/Die.tsx
+++ My React Project/src/components/game/Die.tsx
@@ -1,18 +1,26 @@
+import clsx from "clsx"
+
 type DieProps={
     value: number,
-    isHeld: boolean
+    isHeld: boolean,
+    hold: ()=>void
 }
 
 function Die(props: DieProps){
 
      function handleClick(){
-    
-      console.log(`Die ${props.value} clicked`)
-
-    
+      console.log(`Die ${props.value} clicked`)  
+      props.hold()
 }
+
     return(
-        <div className="die mx-2 my-5 p-10" onClick={handleClick}>
+        <div onClick={handleClick} 
+        
+        className={clsx(
+        "die mx-2 my-5 p-10 rounded-lg cursor-pointer transition-all",
+        props.isHeld ? "bg-green-400 text-white" : "bg-gray-500"
+      )}
+        >
             {props.value}
 
         </div>
